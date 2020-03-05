966764a24691fb8136689cbafb07512e0760252e
Charleszoryez <charles.zoryez@gmail.com>
2020-03-05 18:53:02 -0300

    Comienzo de estructura de base de datos. clase de persona (registrar un usuario) clase de eventos (registrar un evento) Creacion de las tablas con peewee

--- ComienzoQuePinto.py
+++ ComienzoQuePinto.py
@@ -1,1 +1,31 @@
-Esto es una prueba desde atom
+from peewee import*
+
+db = SqliteDatabase('quePintoUser.db')
+
+class Person(Model):
+    name = CharField()
+    dni = TextField()
+    email = TextField()
+    password = TextField()
+
+    class Meta:
+        database = db
+
+class Event(Model):
+    nameEvent = CharField()
+    organizer = CharField()
+    category = CharField()
+    description = TextField()
+    galery =
+    city = CharField()
+    department = CharField()
+    country = CharField()
+    gpslength =
+    gpslatitude =
+    startDate =
+    endingDate = 
+
+
+def create_and_connect():
+    db.connect()
+    db.create_tables([Person],safe=True)
